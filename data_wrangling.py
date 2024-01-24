76d8c7a25aa0fe69344b26f1f3d87d4b690e375d
mgoldkuhle <m.j.goldkuhle@lumc.nl>
2024-01-24 23:01:42 +0100

    combine t1ce and t2 measures

--- data_wrangling.py
+++ data_wrangling.py
@@ -4,29 +4,43 @@ import pandas as pd
 
 # Load the CSV file
 load_path = 'C:/Users/mjgoldkuhle/ownCloud/LUMC/data/selected_features'
-growths = pd.read_csv(os.path.join(load_path, 't2_3d_all_growth.csv'))
+growths = pd.read_csv(os.path.join(load_path, 'WORCschwannoma.csv'))
 
 # Iterate over each row in the CSV
 for index, row in growths.iterrows():
-    patient_path = os.path.join('//vf-DataSafe/DataSafe$/div2/radi/Brughoek_predict_1255/01_followup_cleanedup/followup_part1', row['patient'])
+    patient_id = row['Patient']
+    patient_path = os.path.join('//vf-DataSafe/DataSafe$/div2/radi/Brughoek_predict_1255/01_followup_cleanedup/followup_part1', patient_id)
 
-    # only copy scans from the latest date
+    # only copy scans from the first date
     dates = [folder for folder in os.listdir(patient_path) if os.path.isdir(os.path.join(patient_path, folder))]
-    latest_date = max(dates)
+    first_date = min(dates)
+    # sequence of this patient
+    sequence = row['sequence']
+    # check if folder contains sequence scan, otherwise take next date and check again
+    while not os.path.exists(os.path.join(patient_path, first_date, patient_id.split('_')[1] + '_' + first_date + '_' + sequence + '.nii.gz')):
+        print(os.path.join(patient_path, first_date, patient_id + '_' + first_date + '_' + sequence +'.nii.gz'))
+        dates.remove(first_date)
+        first_date = min(dates)
 
     # Construct the source and destination paths
-    source_path = os.path.join(patient_path, latest_date)
-    img_destination_path = os.path.join('D:/repos/WORC/WORCTutorial/Data/t2schwannoma', row['patient'], 'image.nii.gz')
-    mask_destination_path = os.path.join('D:/repos/WORC/WORCTutorial/Data/t2schwannoma', row['patient'], 'mask.nii.gz')
+    source_path = os.path.join(patient_path, first_date)
+    img_destination_path = os.path.join('D:/repos/WORC/WORCTutorial/Data/schwannoma', row['Patient'], 'image.nii.gz')
+    mask_destination_path = os.path.join('D:/repos/WORC/WORCTutorial/Data/schwannoma', row['Patient'], 'mask.nii.gz')
     # create directories if they don't exist
-    os.makedirs(os.path.dirname(os.path.join('D:/repos/WORC/WORCTutorial/Data/t2schwannoma', row['patient'])), exist_ok=True)
+    os.makedirs(os.path.dirname(os.path.join('D:/repos/WORC/WORCTutorial/Data/schwannoma', row['Patient'])), exist_ok=True)
 
-    # Find the files ending with t2.nii.gz and t2_seg.nii.gz
+    # Find the files ending with <sequence>.nii.gz and <sequence>_seg.nii.gz
     for file in os.listdir(source_path):
-        if file.endswith('t2.nii.gz'):
+        if file.endswith(f'{sequence}.nii.gz'):
+            # remove old image and mask files if they exist
+            if os.path.exists(img_destination_path):
+                os.remove(img_destination_path)
             # Copy the image file
             shutil.copy(os.path.join(source_path, file), img_destination_path)
-        elif file.endswith('t2_seg.nii.gz'):
+        elif file.endswith(f'{sequence}_seg.nii.gz'):
+            # remove old image and mask files if they exist
+            if os.path.exists(mask_destination_path):
+                os.remove(mask_destination_path)
             # Copy the mask file and rename it to mask.nii.gz
             shutil.copy(os.path.join(source_path, file), mask_destination_path)
             
--- growth_rates.py
+++ growth_rates.py
@@ -6,17 +6,25 @@ import matplotlib.pyplot as plt
 
 # import VS diameters
 load_path = 'C:/Users/mjgoldkuhle/ownCloud/LUMC/data/selected_features'
-diameters = pd.read_csv(os.path.join(load_path, 't1ce_3d_all.csv'))
+diameters_t1ce = pd.read_csv(os.path.join(load_path, 't1ce_3d_all.csv'))
+diameters_t1ce['sequence'] = 't1ce'
+diameters_t2 = pd.read_csv(os.path.join(load_path, 't2_3d_all.csv'))
+diameters_t2['sequence'] = 't2'
+diameters = pd.concat([diameters_t1ce, diameters_t2], ignore_index=True)
 
-# segmentation code produced some 0 measures if there's no t2 scan or a bad segmentation
+# segmentation code produced some 0 measures if there's no scan or a bad segmentation
 diameters = diameters[diameters['max_diameter'] != 0]
-# remove patients with less than 3 measurements
-diameters = diameters.groupby('patient').filter(lambda x: len(x) > 2)
+# remove patients with less than 3 measurements in either sequence
+diameters = diameters.groupby(['patient', 'sequence']).filter(lambda x: len(x) >= 3)
+# if there are t1ce measurements for a patient, remove t2 measurements. t1ce seem to be more accurate
+filtered_df = diameters.groupby('patient').apply(lambda x: x[x['sequence'] == 't1ce'] if ('t1ce' in x['sequence'].values and 't2' in x['sequence'].values) else x).reset_index(drop=True)
+diameters = filtered_df
+# print unique patients
+print('Number of patients with three or more measures: {}'.format(len(diameters['patient'].unique())))
 
 # print how many patients have three or more measures
 print('Number of patients with two or more measures: {}'.format(len(diameters['patient'].unique())))
 # 49 patients with two or more measures. 36 patients with three or more measures.
-
 
 # parse date and transform to days since 01.01.1995
 diameters['date'] = pd.to_datetime(diameters['date'])
@@ -80,14 +88,14 @@ plt.show()
 
 
 # calculate growth for each patient
-growths = {'patient': [], 'growth': []}
+growths = {'Patient': [], 'growth': []}
 for patient in diameters_interpol['patient'].unique():
     patient_df = diameters_interpol[diameters_interpol['patient'] == patient]
     # growth rate as the difference between the max diameter at the last day and the max diameter 365 days before
     # growth = patient_df['max_diameter'].iloc[-1] - patient_df['max_diameter'].iloc[-365]
     # growth rate as the difference between the max diameter at the last day and the max diameter at the first day
     growth = patient_df['max_diameter'].iloc[-1] - patient_df['max_diameter'].iloc[0]
-    growths['patient'].append(patient)
+    growths['Patient'].append(patient)
     growths['growth'].append(growth)
 growths = pd.DataFrame(growths)
 
@@ -101,7 +109,7 @@ plt.show()
 
 # plot growth curves of patients with growth rates above 2 mm
 plt.figure()
-for patient in growths[growths['growth'] > 2]['patient']:
+for patient in growths[growths['growth'] > 2]['Patient']:
     patient_df = diameters_interpol[diameters_interpol['patient'] == patient]
     plt.plot(patient_df['days'], patient_df['max_diameter'], label=patient)
 plt.legend()
@@ -110,10 +118,11 @@ plt.ylabel('Max diameter [mm]')
 plt.title('Max diameter over time per patient')
 plt.show()
 
-# save csv with patient, growth rate, days between first and last measurement and boolean for growth rate above 2 mm
+# save csv with patient, growth rate, days between first and last measurement and boolean for growth rate above 2 mm and sequence
 growths['days'] = (diameters.groupby('patient')['days'].max() - diameters.groupby('patient')['days'].min()).values
-growths['growth_above_2mm'] = growths['growth'] > 2
-growths.to_csv(os.path.join(load_path, 't1ce_3d_all_growth.csv'), index=False)
-
+growths['above_2mm'] = growths['growth'] > 2
+growths['sequence'] = diameters.groupby('patient')['sequence'].first().values
+growths.to_csv(os.path.join(load_path, 'WORCschwannoma.csv'), index=False)
+print('Saved growth rates to following file: ' + os.path.join(load_path, 'WORCschwannoma.csv'))
 
 
